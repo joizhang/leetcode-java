b7c5ad023f9d3273f78dc6ecfcd539f5d1d0f89d
joizhang <zxlwhom@outlook.com>
2023-01-17 22:01:29 +0800

    feat: 21. 合并两个有序链表

---
+++ src/main/java/com/joizhang/lc/linkedlist/Q21MergeTwoSortedLists.java
@@ -0,0 +1,36 @@
+package com.joizhang.lc.linkedlist;
+
+public class Q21MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode dummy = new ListNode(0);
+        ListNode move = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                move.next = list1;
+                list1 = list1.next;
+            } else {
+                move.next = list2;
+                list2 = list2.next;
+            }
+            move = move.next;
+        }
+        if (list1 == null) {
+            move.next = list2;
+        } else {
+            move.next = list1;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Q21MergeTwoSortedLists test = new Q21MergeTwoSortedLists();
+        ListNode l1 = new ListNode(new int[]{1, 2, 4});
+        ListNode l2 = new ListNode(new int[]{1, 3, 4});
+        System.out.println(test.mergeTwoLists(l1, l2).toString());
+
+        l2 = new ListNode(new int[]{0});
+        System.out.println(test.mergeTwoLists(null, l2).toString());
+    }
+}
