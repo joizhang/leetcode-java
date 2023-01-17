dd8541f87f1c2e032c165b818ccf12ff58294889
joizhang <zxlwhom@outlook.com>
2023-01-17 21:29:24 +0800

    feat: 剑指 Offer 09. 用两个栈实现队列

---
+++ src/main/java/com/joizhang/lcof/queue_stack/Offer09.java
@@ -0,0 +1,49 @@
+package com.joizhang.lcof.queue_stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class CQueue {
+
+    private final Deque<Integer> stackIn;
+    private final Deque<Integer> stackOut;
+
+    public CQueue() {
+        stackIn = new ArrayDeque<>();
+        stackOut = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        stackIn.push(value);
+    }
+
+    public int deleteHead() {
+        if (!stackOut.isEmpty()) {
+            return stackOut.pop();
+        }
+        if (stackIn.isEmpty()) {
+            return -1;
+        }
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        return stackOut.pop();
+    }
+}
+
+public class Offer09 {
+    public static void main(String[] args) {
+        CQueue queue = new CQueue();
+        queue.appendTail(3);
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+
+        queue = new CQueue();
+        System.out.println(queue.deleteHead());
+        queue.appendTail(5);
+        queue.appendTail(2);
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+    }
+}
