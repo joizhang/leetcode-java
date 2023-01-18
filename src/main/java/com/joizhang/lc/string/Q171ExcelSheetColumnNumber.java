4b25483a50b354d7417d33eda014107d1681463d
joizhang <zxlwhom@outlook.com>
2023-01-18 20:17:05 +0800

    feat: 171. Excel 表列序号

---
+++ src/main/java/com/joizhang/lc/string/Q171ExcelSheetColumnNumber.java
@@ -0,0 +1,19 @@
+package com.joizhang.lc.string;
+
+public class Q171ExcelSheetColumnNumber {
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+        for (char c : columnTitle.toCharArray()) {
+            int temp = c - 'A' + 1;
+            res = res * 26 + temp;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Q171ExcelSheetColumnNumber test = new Q171ExcelSheetColumnNumber();
+        System.out.println(test.titleToNumber("A"));
+        System.out.println(test.titleToNumber("AB"));
+        System.out.println(test.titleToNumber("ZY"));
+    }
+}
