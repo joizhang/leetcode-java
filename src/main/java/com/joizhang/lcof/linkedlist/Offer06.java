2d4a6f4e101c2411b67f8e9d28d8ebff773b36af
joizhang <zxlwhom@outlook.com>
2023-01-19 18:30:24 +0800

    feat: 剑指 Offer 06. 从尾到头打印链表

---
+++ src/main/java/com/joizhang/lcof/linkedlist/Offer06.java
@@ -0,0 +1,27 @@
+package com.joizhang.lcof.linkedlist;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Offer06 {
+    public int[] reversePrint(ListNode head) {
+        if (head == null) return new int[]{};
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Offer06 test = new Offer06();
+        ListNode head = new ListNode(new int[]{1, 3, 2});
+        System.out.println(Arrays.toString(test.reversePrint(head)));
+    }
+}
