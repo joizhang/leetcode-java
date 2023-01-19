7ff21f0ef42f0a3b9cf564b15f6a1b76aff5ca5f
joizhang <zxlwhom@outlook.com>
2023-01-19 18:31:04 +0800

    feat: 剑指 Offer 35. 复杂链表的复制

---
+++ src/main/java/com/joizhang/lcof/linkedlist/Offer35.java
@@ -0,0 +1,36 @@
+package com.joizhang.lcof.linkedlist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class Offer35 {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
