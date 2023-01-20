2738715fa7d7ee9a65666ffae71990ca40ed8a9c
joizhang <zxlwhom@outlook.com>
2023-01-20 21:55:51 +0800

    feat: 20. 有效的括号

---
+++ src/main/java/com/joizhang/lc/queue_stack/Q020ValidParentheses.java
@@ -0,0 +1,38 @@
+package com.joizhang.lc.queue_stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 20. 有效的括号
+ */
+public class Q020ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else{
+                if (c == ')' && (stack.isEmpty() || stack.peek() != '(')) {
+                    return false;
+                } else if (c == '}' && (stack.isEmpty() || stack.peek() != '{')) {
+                    return false;
+                } else if (c == ']' && (stack.isEmpty() || stack.peek() != '[')) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Q020ValidParentheses test = new Q020ValidParentheses();
+        System.out.println(test.isValid("()"));
+        System.out.println(test.isValid("()[]{}"));
+        System.out.println(test.isValid("(]"));
+    }
+}
