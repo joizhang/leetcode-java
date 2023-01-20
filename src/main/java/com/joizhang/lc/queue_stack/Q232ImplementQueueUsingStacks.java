b51e81da94ef9a1c3e8e9a0af25c0446f70eaa87
joizhang <zxlwhom@outlook.com>
2023-01-20 21:18:34 +0800

    feat: 232. 用栈实现队列

---
+++ src/main/java/com/joizhang/lc/queue_stack/Q232ImplementQueueUsingStacks.java
@@ -0,0 +1,64 @@
+package com.joizhang.lc.queue_stack;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 232. 用栈实现队列
+ */
+class MyQueue {
+
+    private final Deque<Integer> stackIn;
+    private final Deque<Integer> stackOut;
+
+    public MyQueue() {
+        stackIn = new ArrayDeque<>();
+        stackOut = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    public int pop() {
+        if (!stackOut.isEmpty()) {
+            return stackOut.pop();
+        }
+        if (stackIn.isEmpty()) {
+            return -1;
+        }
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        return stackOut.pop();
+    }
+
+    public int peek() {
+        if (!stackOut.isEmpty()) {
+            return stackOut.peek();
+        }
+        if (stackIn.isEmpty()) {
+            return -1;
+        }
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        return stackOut.peek();
+    }
+
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+}
+
+public class Q232ImplementQueueUsingStacks {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop()); // return 1, queue is [2]
+        System.out.println(myQueue.empty()); // return false
+    }
+}
