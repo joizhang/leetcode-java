3bd76d55733ac9eb07b0a4ed474c51743f98a26a
joizhang <zxlwhom@outlook.com>
2023-01-20 18:47:37 +0800

    feat: 剑指 Offer 03. 数组中重复的数字

---
+++ src/main/java/com/joizhang/lcof/array/Offer03.java
@@ -0,0 +1,54 @@
+package com.joizhang.lcof.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 剑指 Offer 03. 数组中重复的数字
+ */
+public class Offer03 {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return -1;
+    }
+
+    public int findRepeatNumber2(int[] nums) {
+        assert nums.length >= 2;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public int findRepeatNumber3(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (i == nums[i]) {
+                i++;
+            } else {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                } else {
+                    int temp = nums[i];
+                    nums[i] = nums[temp];
+                    nums[temp] = temp;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
