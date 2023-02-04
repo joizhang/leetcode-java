80682f80916a5da161497e84715edd3f365ced96
joizhang <zxlwhom@outlook.com>
2023-02-04 20:32:04 +0800

    feat: 剑指 Offer 04. 二维数组中的查找

--- src/main/java/com/joizhang/lcof/binary_search/Offer04.java
+++ src/main/java/com/joizhang/lcof/binary_search/Offer04.java
@@ -6,10 +6,8 @@ package com.joizhang.lcof.binary_search;
 public class Offer04 {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if (matrix.length == 0 || matrix[0].length == 0)return false;
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int i = 0;
-        int j = m - 1;
+        int n = matrix.length, m = matrix[0].length;
+        int i = 0, j = m - 1;
         while (i < n && j >= 0) {
             if (matrix[i][j] == target) {
                 return true;
