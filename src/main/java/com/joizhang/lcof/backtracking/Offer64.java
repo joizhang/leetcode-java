67cea395557e4fdbc4f20c28fe5498ee3798f6ad
joizhang <zxlwhom@outlook.com>
2023-02-04 21:12:26 +0800

    feat: 剑指 Offer 64. 求1+2+…+n

---
+++ src/main/java/com/joizhang/lcof/backtracking/Offer64.java
@@ -0,0 +1,18 @@
+package com.joizhang.lcof.backtracking;
+
+/**
+ * 剑指 Offer 64. 求1+2+…+n
+ */
+public class Offer64 {
+    int res = 0;
+    public int sumNums(int n) {
+        boolean x = n > 1 && sumNums(n - 1) > 0;
+        res += n;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Offer64 test = new Offer64();
+        System.out.println(test.sumNums(9));
+    }
+}
