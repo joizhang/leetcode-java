ee8654a4623e68306131fd882734d3886f364ee6
joizhang <zxlwhom@outlook.com>
2023-02-07 13:55:12 +0800

    feat: 137. 只出现一次的数字 II

--- src/main/java/com/joizhang/lc/math/Q137SingleNumberII.java
+++ src/main/java/com/joizhang/lc/math/Q137SingleNumberII.java
@@ -1,6 +1,10 @@
-package com.joizhang.lc.array;
+package com.joizhang.lc.math;
 
+/**
+ * 137. 只出现一次的数字 II
+ */
 public class Q137SingleNumberII {
+    // 遍历统计
     public int singleNumber(int[] nums) {
         // 记录所有数字的各二进制位的1的出现次数
         int[] counts = new int[32];
@@ -13,7 +17,7 @@ public class Q137SingleNumberII {
         int res = 0, m = 3;
         for (int i = 0; i < 32; i++) {
             res <<= 1;
-            res |= counts[31 - i] % m;
+            res |= counts[31 - i] % m; // 恢复第 i 位的值到 res
         }
         return res;
     }
