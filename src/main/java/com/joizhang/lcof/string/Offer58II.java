91ec271b788bc7b9b79f1c3482696e511305394e
joizhang <zxlwhom@outlook.com>
2023-02-07 16:50:05 +0800

    feat: 剑指 Offer 58 - II. 左旋转字符串

--- src/main/java/com/joizhang/lcof/string/Offer58II.java
+++ src/main/java/com/joizhang/lcof/string/Offer58II.java
@@ -3,7 +3,7 @@ package com.joizhang.lcof.string;
 /**
  * 剑指 Offer 58 - II. 左旋转字符串
  */
-public class Offer58 {
+public class Offer58II {
     public String reverseLeftWords(String s, int n) {
         assert 1 <= n && n < s.length();
         return s.substring(n) + s.substring(0, n);
@@ -29,7 +29,7 @@ public class Offer58 {
     }
 
     public static void main(String[] args) {
-        Offer58 test = new Offer58();
+        Offer58II test = new Offer58II();
         System.out.println(test.reverseLeftWords("abcdefg", 2));
         System.out.println(test.reverseLeftWords("lrloseumgh", 6));
 
