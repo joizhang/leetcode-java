aa9e90f2c3014c7c31929fa14a451885b7dd6090
joizhang <zxlwhom@outlook.com>
2023-02-08 16:51:15 +0800

    feat: Add bubble sort

---
+++ src/main/java/com/joizhang/imooc/algorithms/sort/Bubble.java
@@ -0,0 +1,19 @@
+package com.joizhang.imooc.algorithms.sort;
+
+public interface Bubble {
+
+    static <E extends Comparable<E>> void sort(E[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            boolean swapped = false;
+            for (int j = 0; j < i; j++) {
+                if (Sorts.less(arr[j + 1], arr[j])) {
+                    Sorts.exchange(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+}
