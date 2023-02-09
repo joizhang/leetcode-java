68ed6d6468ff8d13077bb4ae0b7eebb18697e3bc
joizhang <zxlwhom@outlook.com>
2023-02-09 20:43:15 +0800

    feat: 剑指 Offer 30. 包含min函数的栈

--- src/main/java/com/joizhang/lcof/queue_stack/Offer30.java
+++ src/main/java/com/joizhang/lcof/queue_stack/Offer30.java
@@ -3,52 +3,55 @@ package com.joizhang.lcof.queue_stack;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-class MinStack {
-
-    final private Deque<Integer> stack;
-    final private Deque<Integer> minStack;
-
-    /**
-     * initialize your data structure here.
-     */
-    public MinStack() {
-        stack = new ArrayDeque<>();
-        minStack = new ArrayDeque<>();
-    }
-
-    public void push(int x) {
-        stack.push(x);
-        if (minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x);
-        }
-    }
-
-    public void pop() {
-        if (stack.isEmpty() || minStack.isEmpty()) {
-            return;
-        }
-        int x = stack.pop();
-        if (x == minStack.peek()) {
-            minStack.pop();
-        }
-    }
-
-    public int top() {
-        if (stack.isEmpty()) {
-            throw new RuntimeException("Stack is empty!");
-        }
-        return stack.peek();
-    }
-
-    public int min() {
-        if (minStack.isEmpty()) {
-            throw new RuntimeException("Stack is empty!");
-        }
-        return minStack.peek();
-    }
-}
-
+/**
+ * 剑指 Offer 30. 包含min函数的栈
+ */
 public class Offer30 {
+    static class MinStack {
+
+        final private Deque<Integer> stack;
+        final private Deque<Integer> minStack;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+            stack = new ArrayDeque<>();
+            minStack = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (minStack.isEmpty() || x <= minStack.peek()) {
+                minStack.push(x);
+            }
+        }
+
+        public void pop() {
+            if (stack.isEmpty() || minStack.isEmpty()) {
+                return;
+            }
+            int x = stack.pop();
+            if (x == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            if (stack.isEmpty()) {
+                throw new RuntimeException("Stack is empty!");
+            }
+            return stack.peek();
+        }
+
+        public int min() {
+            if (minStack.isEmpty()) {
+                throw new RuntimeException("Stack is empty!");
+            }
+            return minStack.peek();
+        }
+    }
+
     public static void main(String[] args) {
         MinStack minStack = new MinStack();
         minStack.push(-2);
