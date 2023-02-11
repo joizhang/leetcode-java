5e07601a0585d2bb81f18e84c94c80dbd9e66182
joizhang <zxlwhom@outlook.com>
2023-02-11 11:28:53 +0800

    feat: 剑指 Offer 62. 圆圈中最后剩下的数字

---
+++ src/main/java/com/joizhang/lcof/math/Offer62.java
@@ -0,0 +1,20 @@
+package com.joizhang.lcof.math;
+
+/**
+ * 剑指 Offer 62. 圆圈中最后剩下的数字
+ * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/javajie-jue-yue-se-fu-huan-wen-ti-gao-su-ni-wei-sh/
+ */
+public class Offer62 {
+    public int lastRemaining(int n, int m) {
+        int x = 0;
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Offer62 test = new Offer62();
+        System.out.println(test.lastRemaining(5, 3));
+    }
+}
