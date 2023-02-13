82934291514d6e2fdcc67e179a333d4d07004d4c
joizhang <zxlwhom@outlook.com>
2023-02-13 20:41:39 +0800

    feat: 225. 用队列实现栈

--- src/main/java/com/joizhang/lc/queue_stack/Q225ImplementStackUsingQueues.java
+++ src/main/java/com/joizhang/lc/queue_stack/Q225ImplementStackUsingQueues.java
@@ -6,46 +6,46 @@ import java.util.Deque;
 /**
  * 225. 用队列实现栈
  */
-class MyStack {
-
-    private Deque<Integer> queueA;
-    private Deque<Integer> queueB;
-
-    public MyStack() {
-        queueA = new ArrayDeque<>();
-        queueB = new ArrayDeque<>();
-    }
-
-    public void push(int x) {
-        queueB.offer(x);
-        while (!queueA.isEmpty()) {
-            queueB.offer(queueA.poll());
-        }
-        Deque<Integer> temp = queueB;
-        queueB = queueA;
-        queueA = temp;
-    }
-
-    public int pop() {
-        if (queueA.isEmpty()) {
-            return -1;
-        }
-        return queueA.poll();
-    }
-
-    public int top() {
-        if (queueA.isEmpty()) {
-            return -1;
-        }
-        return queueA.peek();
-    }
-
-    public boolean empty() {
-        return queueA.isEmpty();
-    }
-}
-
 public class Q225ImplementStackUsingQueues {
+    static class MyStack {
+
+        private Deque<Integer> queueA;
+        private Deque<Integer> queueB;
+
+        public MyStack() {
+            queueA = new ArrayDeque<>();
+            queueB = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            queueB.offer(x);
+            while (!queueA.isEmpty()) {
+                queueB.offer(queueA.poll());
+            }
+            Deque<Integer> temp = queueB;
+            queueB = queueA;
+            queueA = temp;
+        }
+
+        public int pop() {
+            if (queueA.isEmpty()) {
+                return -1;
+            }
+            return queueA.poll();
+        }
+
+        public int top() {
+            if (queueA.isEmpty()) {
+                return -1;
+            }
+            return queueA.peek();
+        }
+
+        public boolean empty() {
+            return queueA.isEmpty();
+        }
+    }
+
     public static void main(String[] args) {
         MyStack myStack = new MyStack();
         myStack.push(1);
