7522372501554d74d1d5b3f82577deb5be9b4104
joizhang <zxlwhom@outlook.com>
2023-02-13 20:41:47 +0800

    feat: 232. 用栈实现队列

--- src/main/java/com/joizhang/lc/queue_stack/Q232ImplementQueueUsingStacks.java
+++ src/main/java/com/joizhang/lc/queue_stack/Q232ImplementQueueUsingStacks.java
@@ -7,52 +7,52 @@ import java.util.Deque;
 /**
  * 232. 用栈实现队列
  */
-class MyQueue {
+public class Q232ImplementQueueUsingStacks {
+    static class MyQueue {
 
-    private final Deque<Integer> stackIn;
-    private final Deque<Integer> stackOut;
+        private final Deque<Integer> stackIn;
+        private final Deque<Integer> stackOut;
 
-    public MyQueue() {
-        stackIn = new ArrayDeque<>();
-        stackOut = new ArrayDeque<>();
-    }
+        public MyQueue() {
+            stackIn = new ArrayDeque<>();
+            stackOut = new ArrayDeque<>();
+        }
 
-    public void push(int x) {
-        stackIn.push(x);
-    }
+        public void push(int x) {
+            stackIn.push(x);
+        }
 
-    public int pop() {
-        if (!stackOut.isEmpty()) {
+        public int pop() {
+            if (!stackOut.isEmpty()) {
+                return stackOut.pop();
+            }
+            if (stackIn.isEmpty()) {
+                return -1;
+            }
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
             return stackOut.pop();
         }
-        if (stackIn.isEmpty()) {
-            return -1;
-        }
-        while (!stackIn.isEmpty()) {
-            stackOut.push(stackIn.pop());
-        }
-        return stackOut.pop();
-    }
 
-    public int peek() {
-        if (!stackOut.isEmpty()) {
+        public int peek() {
+            if (!stackOut.isEmpty()) {
+                return stackOut.peek();
+            }
+            if (stackIn.isEmpty()) {
+                return -1;
+            }
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
             return stackOut.peek();
         }
-        if (stackIn.isEmpty()) {
-            return -1;
+
+        public boolean empty() {
+            return stackIn.isEmpty() && stackOut.isEmpty();
         }
-        while (!stackIn.isEmpty()) {
-            stackOut.push(stackIn.pop());
-        }
-        return stackOut.peek();
     }
 
-    public boolean empty() {
-        return stackIn.isEmpty() && stackOut.isEmpty();
-    }
-}
-
-public class Q232ImplementQueueUsingStacks {
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
         myQueue.push(1); // queue is: [1]
