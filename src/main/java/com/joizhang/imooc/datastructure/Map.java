b83bd2529d0a963eb124eb21424a4f95455b7140
joizhang <zxlwhom@outlook.com>
2023-02-13 20:05:56 +0800

    feat: add Map

--- src/main/java/com/joizhang/imooc/datastructure/Map.java
+++ src/main/java/com/joizhang/imooc/datastructure/Map.java
@@ -6,11 +6,32 @@ public interface Map<K, V> {
 
     boolean isEmpty();
 
+    /**
+     * 插入键值对到Map中
+     * @param key key
+     * @param value value
+     */
     void put(K key, V value);
 
+    /**
+     * 获得键对应的值
+     * @param key key
+     * @return 键对应的值
+     */
     V get(K key);
 
+    /**
+     * 如果Map中包含该键值对则返回true
+     * @param key key
+     * @return 如果Map中包含该键值对则返回true
+     */
     boolean containsKey(K key);
 
+    /**
+     * 如果键值对存在则删除健值对
+     * @param key key
+     * @return 键对应的值，如果不存在返回null
+     */
     V remove(K key);
+
 }
