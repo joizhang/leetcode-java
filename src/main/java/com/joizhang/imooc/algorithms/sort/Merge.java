3d2b66a75e2b69debcdcd43b192834b553572bed
joizhang <zxlwhom@outlook.com>
2023-02-14 18:53:02 +0800

    feat: 自顶向下归并排序

--- src/main/java/com/joizhang/imooc/algorithms/sort/Merge.java
+++ src/main/java/com/joizhang/imooc/algorithms/sort/Merge.java
@@ -1,8 +1,7 @@
 package com.joizhang.imooc.algorithms.sort;
 
 /**
- * 自顶向下归并排序。
- * 将一个大数组分成两个小数组去求解。因为每次都将问题对半分成两个子问题，
+ * 自顶向下归并排序。将一个大数组分成两个小数组去求解。因为每次都将问题对半分成两个子问题，
  * 这种对半分的算法复杂度一般为 O(NlogN)。
  */
 public interface Merge {
