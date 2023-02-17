d338eb0ca2428aafaad4bae89f196fa93014d323
joizhang <zxlwhom@outlook.com>
2023-02-17 22:20:24 +0800

    160. 相交链表

---
+++ src/main/java/com/joizhang/lc/linkedlist/Q160IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,16 @@
+package com.joizhang.lc.linkedlist;
+
+/**
+ * 160. 相交链表
+ */
+public class Q160IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA != null ? pA.next : headB;
+            pB = pB != null ? pB.next : headA;
+        }
+        return pA;
+    }
+}
