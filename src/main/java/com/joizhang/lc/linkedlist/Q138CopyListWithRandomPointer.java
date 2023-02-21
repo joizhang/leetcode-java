11a05aa5a2ff7b42cc8003a69fbe48c439b3ef53
joizhang <zxlwhom@outlook.com>
2023-02-21 22:25:38 +0800

    feat: 138. 复制带随机指针的链表

---
+++ src/main/java/com/joizhang/lc/linkedlist/Q138CopyListWithRandomPointer.java
@@ -0,0 +1,39 @@
+package com.joizhang.lc.linkedlist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 138. 复制带随机指针的链表
+ */
+public class Q138CopyListWithRandomPointer {
+    // Definition for a Node.
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null)return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
