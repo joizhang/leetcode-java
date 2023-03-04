4302b419d38f4e3f6a38d7fb8a6af7fc07578a90
joizhang <zxlwhom@outlook.com>
2023-03-04 15:58:07 +0800

    feat: 5. 最长回文子串

--- src/main/java/com/joizhang/lc/string/Q005LongestPalindromicSubstring.java
+++ src/main/java/com/joizhang/lc/string/Q005LongestPalindromicSubstring.java
@@ -1,5 +1,8 @@
-package com.joizhang.lc.dp.string;
+package com.joizhang.lc.string;
 
+/**
+ * 5. 最长回文子串
+ */
 public class Q005LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         if (s == null || s.isEmpty()) return "";
@@ -28,12 +31,32 @@ public class Q005LongestPalindromicSubstring {
     }
 
     public String longestPalindrome2(String s) {
-        return s;
+        if (s == null || s.length() < 2) return s;
+        int strLen = s.length();
+        // 最长回文串的起点，最长回文串的终点
+        int maxStart = 0, maxEnd = 0, maxLen = 1;
+        boolean[][] dp = new boolean[strLen][strLen];
+        for (int r = 1; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+                    }
+                }
+            }
+        }
+        return s.substring(maxStart, maxEnd + 1);
     }
 
     public static void main(String[] args) {
         Q005LongestPalindromicSubstring test = new Q005LongestPalindromicSubstring();
         System.out.println(test.longestPalindrome("babad"));
         System.out.println(test.longestPalindrome("cbbd"));
+
+        System.out.println(test.longestPalindrome2("babad"));
+        System.out.println(test.longestPalindrome2("cbbd"));
     }
 }
