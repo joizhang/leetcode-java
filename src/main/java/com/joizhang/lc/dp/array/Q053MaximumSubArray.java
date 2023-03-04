a72c2ee6c85a092afbbf9cf7e564d9ffa1c3ddfe
joizhang <zxlwhom@outlook.com>
2023-03-04 20:23:05 +0800

    feat: 53. 最大子数组和

--- src/main/java/com/joizhang/lc/dp/array/Q053MaximumSubArray.java
+++ src/main/java/com/joizhang/lc/dp/array/Q053MaximumSubArray.java
@@ -1,9 +1,9 @@
 package com.joizhang.lc.dp.array;
 
 /**
- *
+ * 53. 最大子数组和
  */
-public class Q53MaximumSubArray {
+public class Q053MaximumSubArray {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
         int ans = nums[0], dp = nums[0];
@@ -16,7 +16,7 @@ public class Q53MaximumSubArray {
     }
 
     public static void main(String[] args) {
-        Q53MaximumSubArray test = new Q53MaximumSubArray();
+        Q053MaximumSubArray test = new Q053MaximumSubArray();
         System.out.println(test.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         System.out.println(test.maxSubArray(new int[]{1}));
         System.out.println(test.maxSubArray(new int[]{5, 4, -1, 7, 8}));
