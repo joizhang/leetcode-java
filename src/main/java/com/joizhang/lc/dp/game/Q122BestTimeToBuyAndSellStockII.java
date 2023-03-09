632036fda033fc247ec85214b95c452ea17462ce
joizhang <zxlwhom@outlook.com>
2023-03-09 09:59:17 +0800

    feat: 122. Best Time to Buy and Sell Stock II

--- src/main/java/com/joizhang/lc/dp/game/Q122BestTimeToBuyAndSellStockII.java
+++ src/main/java/com/joizhang/lc/dp/game/Q122BestTimeToBuyAndSellStockII.java
@@ -3,7 +3,7 @@ package com.joizhang.lc.dp.game;
 /**
  * 122. Best Time to Buy and Sell Stock II
  */
-public class Q121BestTimeToBuyAndSellStockII {
+public class Q122BestTimeToBuyAndSellStockII {
     public int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
@@ -13,7 +13,7 @@ public class Q121BestTimeToBuyAndSellStockII {
     }
 
     public static void main(String[] args) {
-        Q121BestTimeToBuyAndSellStockII test = new Q121BestTimeToBuyAndSellStockII();
+        Q122BestTimeToBuyAndSellStockII test = new Q122BestTimeToBuyAndSellStockII();
         System.out.println(test.maxProfit(new int[]{7,1,5,3,6,4}));
         System.out.println(test.maxProfit(new int[]{1,2,3,4,5}));
         System.out.println(test.maxProfit(new int[]{7,6,4,3,1}));
