cf138df005a803f441e199a97f5435c8c157f5e2
joizhang <zxlwhom@outlook.com>
2023-03-28 22:23:24 +0800

    feat: 31. 下一个排列

--- src/main/java/com/joizhang/lc/backtracking/Q031NextPermutation.java
+++ src/main/java/com/joizhang/lc/backtracking/Q031NextPermutation.java
@@ -4,11 +4,39 @@ import java.util.Arrays;
 
 /**
  * 31. 下一个排列
+ * [1, 2, 3, 4, 5, 6]
+ * [1, 2, 3, 4, 6, 5]
+ * [1, 2, 3, 5, 4, 6]
+ * [1, 2, 3, 5, 6, 4]
+ * [1, 2, 3, 6, 4, 5]
+ * [1, 2, 3, 6, 5, 4]
+ * [1, 2, 4, 3, 5, 6]
+ * [1, 2, 4, 3, 6, 5]
+ * [1, 2, 4, 5, 3, 6]
+ * [1, 2, 4, 5, 6, 3]
  */
 public class Q031NextPermutation {
+    /**
+     * 举个例子：
+     * 比如 nums = [1,2,7,4,3,1]，下一个排列是什么？
+     * <p>
+     * 我们找到第一个最大索引是 nums[1] = 2
+     * <p>
+     * 再找到第二个最大索引是 nums[4] = 3
+     * <p>
+     * 交换，nums = [1,3,7,4,2,1];
+     * <p>
+     * 翻转，nums = [1,3,1,2,4,7]
+     * <p>
+     * 作者：powcai
+     * 链接：https://leetcode.cn/problems/next-permutation/solution/xia-yi-ge-pai-lie-by-powcai/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length == 0) return;
         int firstIndex = -1;
+        // 1. 先找出最大的索引 firstIndex 满足 nums[firstIndex] < nums[firstIndex+1]，如果不存在，就翻转整个数组
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 firstIndex = i;
@@ -20,6 +48,7 @@ public class Q031NextPermutation {
             return;
         }
 
+        // 2. 再找出另一个最大索引 secondIndex 满足 nums[secondIndex] > nums[firstIndex]；
         int secondIndex = -1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (nums[i] > nums[firstIndex]) {
@@ -27,8 +56,9 @@ public class Q031NextPermutation {
                 break;
             }
         }
-
+        // 3. 交换 nums[firstIndex] 和 nums[secondIndex]；
         swap(nums, firstIndex, secondIndex);
+        // 4. 最后翻转 nums[firstIndex+1:]。
         reverse(nums, firstIndex + 1, nums.length - 1);
     }
 
