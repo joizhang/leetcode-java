d47da346a9708c1f26ba15283576b548a9afc90e
joizhang <zxlwhom@outlook.com>
2023-05-07 21:22:55 +0800

    review: 剑指 Offer II 024. 反转链表

--- src/main/java/com/joizhang/lc/Solution.java
+++ src/main/java/com/joizhang/lc/Solution.java
@@ -1,39 +1,29 @@
 package com.joizhang.lc;
 
+import com.joizhang.lc.linkedlist.ListNode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
-        List<int[]> res = new ArrayList<>();
-        int[] newInterval = intervals[0];
-        res.add(newInterval);
-        for (int i = 1; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            if (newInterval[1] < interval[0]) {
-                newInterval = intervals[i];
-                res.add(newInterval);
-            } else {
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
-            }
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode dummy = new ListNode(0);
+        ListNode p = head;
+        while (head != null) {
+            head = head.next;
+            p.next = dummy.next;
+            dummy.next = p;
+            p = head;
         }
-        return res.toArray(new int[res.size()][]);
+        return dummy.next;
     }
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        System.out.println(Arrays.deepToString(test.merge(new int[][]{
-                new int[]{1, 3},
-                new int[]{2, 6},
-                new int[]{8, 10},
-                new int[]{15, 18}
-        })));
-        System.out.println(Arrays.deepToString(test.merge(new int[][]{
-                new int[]{1, 4},
-                new int[]{4, 5},
-        })));
+        ListNode head = new ListNode(new int[]{1,2,3,4,5});
+        System.out.println(test.reverseList(head));
     }
 }
