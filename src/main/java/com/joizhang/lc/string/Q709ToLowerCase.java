6c3c195788f0eb96f6f5f7431e47205228ed1bfb
joizhang <zxlwhom@outlook.com>
2023-05-08 21:11:42 +0800

    feat: 709. 转换成小写字母

---
+++ src/main/java/com/joizhang/lc/string/Q709ToLowerCase.java
@@ -0,0 +1,9 @@
+package com.joizhang.lc.string;
+
+public class Q709ToLowerCase {
+
+    public String toLowerCase(String s) {
+        if (s == null || s.length() == 0) return null;
+        return s.toLowerCase();
+    }
+}
