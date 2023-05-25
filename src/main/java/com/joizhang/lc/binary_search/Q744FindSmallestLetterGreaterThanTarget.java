328cae7ac1e5fdb604944cbcf67d00aae13d6ef2
joizhang <zxlwhom@outlook.com>
2023-05-25 12:25:00 +0800

    feat: 744. 寻找比目标字母大的最小字母

---
+++ src/main/java/com/joizhang/lc/binary_search/Q744FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,20 @@
+package com.joizhang.lc.binary_search;
+
+public class Q744FindSmallestLetterGreaterThanTarget {
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        if (letters[letters.length - 1] <= target) return letters[0];
+        int lo = 0, hi = letters.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            // 如果当前下标处的字母大于目标字母，则在当前下标以及当前下标的左侧继续查找，否则在当前下标的右侧继续查找。
+            if (letters[mid] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return letters[lo];
+    }
+
+}
