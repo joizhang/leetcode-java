89d3908ccbd5a33d2cb04c9b122f0433ae7c4b62
joizhang <zxlwhom@outlook.com>
2023-05-26 20:49:46 +0800

    feat: 剑指 Offer 31. 栈的压入、弹出序列

--- src/main/java/com/joizhang/lcof/Solution.java
+++ src/main/java/com/joizhang/lcof/Solution.java
@@ -5,53 +5,24 @@ import java.util.Deque;
 
 public class Solution {
 
-    static class MinStack {
-
-        private final Deque<Integer> stack;
-
-        private final Deque<Integer> minStack;
-
-        /**
-         * initialize your data structure here.
-         */
-        public MinStack() {
-            stack = new ArrayDeque<>();
-            minStack = new ArrayDeque<>();
-        }
-
-        public void push(int x) {
-            stack.push(x);
-            if (minStack.isEmpty() || minStack.peek() >= x) {
-                minStack.push(x);
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        if (pushed == null || popped == null) return false;
+        if (pushed.length != popped.length) return false;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int index = 0;
+        for (int num : pushed) {
+            stack.push(num);
+            while (!stack.isEmpty() && stack.peek().equals(popped[index])) {
+                stack.pop();
+                index++;
             }
         }
-
-        public void pop() {
-            if (stack.isEmpty()) {
-                throw new RuntimeException("Stack is empty");
-            }
-            Integer x = stack.pop();
-            if (x.equals(minStack.peek())) {
-                minStack.pop();
-            }
-        }
-
-        public int top() {
-            if (stack.isEmpty()) {
-                throw new RuntimeException("Stack is empty");
-            }
-            return stack.peek();
-        }
-
-        public int min() {
-            if (minStack.isEmpty()) {
-                throw new RuntimeException("Stack is empty");
-            }
-            return minStack.peek();
-        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
         Solution test = new Solution();
+        System.out.println(test.validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}));
+        System.out.println(test.validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 3, 5, 1, 2}));
     }
 }
