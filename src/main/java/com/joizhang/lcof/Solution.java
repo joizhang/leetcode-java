2b1f4e57500c52e9eb0fdc0d26836bec3013363e
joizhang <zxlwhom@outlook.com>
2023-05-27 20:06:33 +0800

    feat: 剑指 Offer 53 - I. 在排序数组中查找数字 I

--- src/main/java/com/joizhang/lcof/Solution.java
+++ src/main/java/com/joizhang/lcof/Solution.java
@@ -1,28 +1,51 @@
 package com.joizhang.lcof;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Solution {
 
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-        if (pushed == null || popped == null) return false;
-        if (pushed.length != popped.length) return false;
-        Deque<Integer> stack = new ArrayDeque<>();
-        int index = 0;
-        for (int num : pushed) {
-            stack.push(num);
-            while (!stack.isEmpty() && stack.peek().equals(popped[index])) {
-                stack.pop();
-                index++;
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return 0;
+        int posFirst = findFirstPos(nums, target);
+        if (posFirst == -1) return 0;
+        int posLast = findLastPos(nums, target);
+        return posLast - posFirst + 1;
+    }
+
+    private int findLastPos(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            if (target < nums[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
             }
         }
-        return stack.isEmpty();
+        if (nums[lo] == target) {
+            return lo;
+        }
+        return -1;
+    }
+
+    private int findFirstPos(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target > nums[mid]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        if (nums[lo] == target) {
+            return lo;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        System.out.println(test.validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}));
-        System.out.println(test.validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 3, 5, 1, 2}));
+        System.out.println(test.search(new int[]{5,7,7,8,8,10}, 8));
+        System.out.println(test.search(new int[]{5,7,7,8,8,10}, 7));
+        System.out.println(test.search(new int[]{5,7,7,8,8,10}, 6));
     }
 }
