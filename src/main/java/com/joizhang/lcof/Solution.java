0c93d58cf919e9c17193f0967be27cbd01fa90e1
joizhang <zxlwhom@outlook.com>
2023-05-28 10:52:00 +0800

    feat: 剑指 Offer 67. 把字符串转换成整数

--- src/main/java/com/joizhang/lcof/Solution.java
+++ src/main/java/com/joizhang/lcof/Solution.java
@@ -2,21 +2,41 @@ package com.joizhang.lcof;
 
 public class Solution {
 
-    public int missingNumber(int[] nums) {
-        int lo = 0, hi = nums.length - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] == mid) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+    public int strToInt(String str) {
+        if (str == null) return 0;
+        char[] s = str.trim().toCharArray();
+        if (s.length == 0) return 0;
+        int symbol = 1, i = 0, ans = 0;
+        if (s[i] == '-') {
+            symbol = -1;
+            i++;
+        } else if (s[i] == '+') {
+            i++;
         }
-        return lo;
+
+        while (i < s.length) {
+            if (!Character.isDigit(s[i])) break;
+            if (s[i] == ' ') {
+                i++;
+                continue;
+            }
+            int num = (s[i] - '0') * symbol;
+            int MAX_VALUE_DIV_10 = Integer.MAX_VALUE / 10;
+            if (ans > MAX_VALUE_DIV_10 || (ans == MAX_VALUE_DIV_10 && num > 7)) {
+                return Integer.MAX_VALUE;
+            }
+            int MIN_VALUE_DIV_10 = Integer.MIN_VALUE / 10;
+            if (ans < MIN_VALUE_DIV_10 || (ans == MIN_VALUE_DIV_10 && num < -8)) {
+                return Integer.MIN_VALUE;
+            }
+            ans = ans * 10 + num;
+            i++;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        System.out.println(test.missingNumber(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 9}));
+        System.out.println(test.strToInt("42"));
     }
 }
