381fdc1356525697fbf085ba8a10a6a4017b91d2
joizhang <zxlwhom@outlook.com>
2023-05-29 20:00:06 +0800

    feat: 剑指 Offer 14- II. 剪绳子 II

--- src/main/java/com/joizhang/lcof/math/Offer14II.java
+++ src/main/java/com/joizhang/lcof/math/Offer14II.java
@@ -4,6 +4,7 @@ package com.joizhang.lcof.math;
  * 剑指 Offer 14- II. 剪绳子 II
  */
 public class Offer14II {
+    // 快速幂求余
     public int cuttingRope(int n) {
         if (n <= 3) return n - 1;
         int b = n % 3, p = 1000000007;
@@ -17,6 +18,27 @@ public class Offer14II {
         if (b == 0) return (int) (rem * 3 % p);
         if (b == 1) return (int) (rem * 4 % p);
         return (int) (rem * 6 % p);
+    }
+
+    // 循环取余
+    public int cuttingRope2(int n) {
+        if (n <= 3) return n - 1;
+        int b = n % 3, p = 1000000007;
+        long ret = 1;
+        // 线段被我们分成以3为大小的小线段个数
+        int lineNums = n / 3;
+        // 从第一段线段开始验算，3的ret次方是否越界。注意是验算lineNums-1次。
+        for (int i = 1; i < lineNums; i++) {
+            ret = 3 * ret % p;
+        }
+
+        if (b == 0) {
+            return (int) (ret * 3 % p);
+        } else if (b == 1) {
+            return (int) (ret * 4 % p);
+        } else {
+            return (int) (ret * 6 % p);
+        }
     }
 
     public static void main(String[] args) {
