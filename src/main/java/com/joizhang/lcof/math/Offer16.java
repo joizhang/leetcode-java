c9de645333568fcd47c735a03759cf97e87363e0
joizhang <zxlwhom@outlook.com>
2023-05-31 19:50:13 +0800

    feat: 剑指 Offer 16. 数值的整数次方

--- src/main/java/com/joizhang/lcof/math/Offer16.java
+++ src/main/java/com/joizhang/lcof/math/Offer16.java
@@ -13,7 +13,6 @@ public class Offer16 {
     public double myPow(double x, int n) {
         if (x == 0) return 0;
         long b = n;
-        double res = 1.0;
         if (b < 0) {
             x = 1 / x;
             b = -b;
